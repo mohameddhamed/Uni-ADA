4d3004b8bb27fb4dd0b71b1f58dfa65a4b541616
mohameddhamed <mohamedus.1970.hm@gmail.com>
2024-11-20 23:41:11 +0100

    final shoes.adb

--- week9/bonus-task/shoes.adb
+++ week9/bonus-task/shoes.adb
@@ -9,80 +9,53 @@ procedure Shoes is
       entry Put_Left_Foot_On_Sand; 
    end Sand;
 
-   task Right_Shoe is
-      entry Can_Step(What : in Boolean);
-   end Right_Shoe;
+   task Right_Shoe;
 
-   task Left_Shoe is
-      entry Can_Step(What : in Boolean);
-   end Left_Shoe;
+   task Left_Shoe;
 
    task body Sand is 
    begin
-      --  Right_Shoe.Can_Step;
 
-      loop
+      for I in 1..10 loop
          select
-            accept Put_Right_Foot_On_Sand do
-                  Put_Line("Right Foot is stepping on the Sand.");
-                  Right_Shoe.Can_Step(False);
-                  Left_Shoe.Can_Step(True);
-            end Put_Right_Foot_On_Sand;
-            or
-            accept Put_Left_Foot_On_Sand do 
-                  Put_Line("Left Foot is stepping on the Sand.");
-                  Left_Shoe.Can_Step(False);
-                  Right_Shoe.Can_Step(True);
+            accept Put_Left_Foot_On_Sand do
+               Put_Line("Iteration " & I'Image);
+               Put_Line("Left Foot is stepping on the Sand.");
+               accept Put_Right_Foot_On_Sand do 
+                     Put_Line("Right Foot is stepping on the Sand.");
+               end Put_Right_Foot_On_Sand;
             end Put_Left_Foot_On_Sand;
+            or
+               delay 5.0;
          end select;
       end loop;
 
    end Sand;
-
-   
    task body Left_Shoe is
    begin
       loop
-        select
-            accept Can_Step(What : in Boolean) do
-               if What = True then
-                  Sand.Put_Left_Foot_On_Sand;
-               end if;
-               --  Put_Line("Left Foot Stepping on Sand");
-            end Can_Step;
-            or 
-               delay 3.0;   
-        end select;
+         Sand.Put_Left_Foot_On_Sand;
+         delay 0.5;   
       end loop;
    exception
       when Tasking_Error =>
-         Put_Line("Closed?");
+         Put_Line("No more steps?");
    end Left_Shoe;
 
    
    task body Right_Shoe is
    begin
       loop
-        select
-            accept Can_Step(What : in Boolean) do
-               if What = True then 
-                  Sand.Put_Right_Foot_On_Sand;
-               end if;
-               --  Put_Line("Right Foot Stepping on Sand");
-            end Can_Step;
-            or 
-               delay 3.0;   
-        end select;
+         Sand.Put_Right_Foot_On_Sand;
+         delay 1.2;   
       end loop;
    exception
       when Tasking_Error =>
-         Put_Line("Closed?");
+         Put_Line("No more steps?");
    end Right_Shoe;
 
    
 begin
-   Right_Shoe.Can_Step(True);
-   --  Left_Shoe.Can_Step;
-      delay 10.0; -- Simulate runtime for the tasks
+   null;
 
 end Shoes;
